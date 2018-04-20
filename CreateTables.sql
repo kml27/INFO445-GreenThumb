55ba67bbbb09fa6e07847e769973b041c31db9c4
Emily Ding <emilyd61@uw.edu>
2018-04-19 21:09:35 -0700

    modified table sheet and make the insert data (need to fix)

--- CreateTables.sql
+++ CreateTables.sql
@@ -1,62 +1,65 @@
+CREATE DATABASE GREEN_THUMB
+USE GREEN_THUMB
+
 CREATE TABLE tblAddress(
-	AddressID INT IDENTITY(1, 1) PRIMARY KEY NOT NULL,
-	StreetAddress varchar(50) NOT NULL,
-	City varchar(50) NOT NULL,
-	[State] char(2) NOT NULL,
-	Zip INT NOT NULL,
+AddressID INT IDENTITY(1, 1) PRIMARY KEY NOT NULL,
+StreetAddress varchar(50) NOT NULL,
+City varchar(50) NOT NULL,
+[State] varchar(50) NOT NULL,
+Zip INT NOT NULL,
 );
 
 CREATE TABLE tblRating(
-	RatingID INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
-	RatingName varchar(10) NOT NULL,
-	RatingDesc varchar(50) ,
+RatingID INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
+RatingName varchar(10) NOT NULL,
+RatingDesc varchar(50) ,
 );
 
 
 CREATE TABLE tblReview(
-	ReviewID INT PRIMARY KEY IDENTITY(1, 1) NOT NULL,
-	CustomerID INT FOREIGN KEY REFERENCES tblCustomer(CustomerID) NOT NULL,
-	SellerID INT FOREIGN KEY REFERENCES tblCustomer(CUstomerID) NOT NULL,
-	RatingID INT FOREIGN KEY REFERENCES tblRating(RatingID) NOT NULL,
-	ReviewTitle varchar(50) NOT NULL,
-	ReviewText varchar(1000) NOT NULL,
+ReviewID INT PRIMARY KEY IDENTITY(1, 1) NOT NULL,
+CustomerID INT FOREIGN KEY REFERENCES tblCustomer(CustomerID) NOT NULL,
+SellerID INT FOREIGN KEY REFERENCES tblCustomer(CustomerID) NOT NULL,
+RatingID INT FOREIGN KEY REFERENCES tblRating(RatingID) NOT NULL,
+ReviewTitle varchar(50) NOT NULL,
+ReviewText varchar(1000) NOT NULL,
 );
 
 CREATE TABLE tblCustomer (
-	CustomerID INT IDENTITY(1,1) PRIMARY KEY not null,
-	FirstName varchar(100) not null,
-	LastName varchar(100) not null,
-	PhoneNumber int not null,
-	Email varchar(100) not null,
-	DOB DATE not null,
-	AddressID INT FOREIGN KEY REFERENCES tblAddress (AddressID) not null
+CustomerID INT IDENTITY(1,1) PRIMARY KEY not null,
+FirstName varchar(100) not null,
+LastName varchar(100) not null,
+[PhoneNumber] varchar (20) not null,
+Email varchar(100) not null,
+DOB DATE not null,
+AddressID INT FOREIGN KEY REFERENCES tblAddress (AddressID) not null
 )
 
 CREATE TABLE tblCustomerCustomerType (
-	CustCustTypeID INT IDENTITY(1,1) PRIMARY KEY not null,
-	CustomerID INT FOREIGN KEY REFERENCES tblCustomer (CustomerID) not null,
-	CustTypeID INT FOREIGN KEY REFERENCES tblCustomerType (CustTypeID) not null,
-	StartDate DATE,
-	EndDate DATE
+CustCustTypeID INT IDENTITY(1,1) PRIMARY KEY not null,
+CustomerID INT FOREIGN KEY REFERENCES tblCustomer (CustomerID) not null,
+CustTypeID INT FOREIGN KEY REFERENCES tblCustomerType (CustTypeID) not null,
+StartDate DATE,
+EndDate DATE
 )
 
 CREATE TABLE tblCustomerType (
-	CustTypeID INT IDENTITY(1,1) PRIMARY KEY not null,
-	CustTypeName varchar(100) not null,
-	CustTypeDesc varchar(100)
+CustTypeID INT IDENTITY(1,1) PRIMARY KEY not null,
+CustTypeName varchar(100) not null,
+CustTypeDesc varchar(100)
 )
 
 CREATE TABLE tblProductType (
-	ProductTypeID INT IDENTITY(1, 1) PRIMARY KEY NOT NULL,
-	ProductTypeName varchar(50) NOT NULL,
-	ProductTypeDesc varchar(500) NULL
+ProductTypeID INT IDENTITY(1, 1) PRIMARY KEY NOT NULL,
+ProductTypeName varchar(50) NOT NULL,
+ProductTypeDesc varchar(500) NULL
 )
 
 CREATE TABLE tblProduct (
-	ProductID INT IDENTITY(1, 1) PRIMARY KEY NOT NULL,
-	ProductTypeID INT FOREIGN KEY REFERENCES tblProductType(ProductTypeID) NOT NULL,
-	ProductName varchar(100) NOT NULL,
-	ProductDesc varchar(500) NULL
+ProductID INT IDENTITY(1, 1) PRIMARY KEY NOT NULL,
+ProductTypeID INT FOREIGN KEY REFERENCES tblProductType(ProductTypeID) NOT NULL,
+ProductName varchar(100) NOT NULL,
+ProductDesc varchar(500) NULL
 )
 
 CREATE TABLE tblUnit
@@ -78,17 +81,17 @@ EndDate DATE NULL,
 UnitID INT FOREIGN KEY REFERENCES tblUnit(UnitID) NOT NULL)
 
 CREATE TABLE tblDetailType(
-	DetailTypeID INT IDENTITY(1, 1) PRIMARY KEY NOT NULL,
-	DetailTypeName varchar(50) NOT NULL,
-	DetailTypeDesc varchar(500) NULL
+DetailTypeID INT IDENTITY(1, 1) PRIMARY KEY NOT NULL,
+DetailTypeName varchar(50) NOT NULL,
+DetailTypeDesc varchar(500) NULL
 )
 
 CREATE TABLE tblDetail(
-	DetailID INT IDENTITY(1, 1) PRIMARY KEY NOT NULL,
-	OfferingID INT FOREIGN KEY REFERENCES tblOffering(OfferingID) NOT NULL,
-	DetailTypeID INT FOREIGN KEY REFERENCES tblDetailType(DetailTypeID) NOT NULL,
-	UnitID INT FOREIGN KEY REFERENCES tblUnit(UnitID) NOT NULL,
-	DetailDesc varchar(500) NOT NULL
+DetailID INT IDENTITY(1, 1) PRIMARY KEY NOT NULL,
+OfferingID INT FOREIGN KEY REFERENCES tblOffering(OfferingID) NOT NULL,
+DetailTypeID INT FOREIGN KEY REFERENCES tblDetailType(DetailTypeID) NOT NULL,
+UnitID INT FOREIGN KEY REFERENCES tblUnit(UnitID) NOT NULL,
+DetailDesc varchar(500) NOT NULL
 )
 
 CREATE TABLE tblOrder
@@ -101,3 +104,4 @@ CREATE TABLE tblLineItem
 OrderID INT FOREIGN KEY REFERENCES tblOrder(OrderID) NOT NULL,
 OfferingID INT FOREIGN KEY REFERENCES tblOffering(OfferingID) NOT NULL,
 Qty INT NOT NULL)
+
---
+++ INSERT_Tables.sql
@@ -0,0 +1,84 @@
+USE [GREEN_THUMB]
+
+SELECT * FROM tblAddress
+
+CREATE TABLE [dbo].[WorkingCustomerData](
+	[CustomerID] int identity (1,1) primary key not null,
+	[CustomerFname] [varchar](50) NULL,
+	[CustomerLname] [varchar](50) NULL,
+	[CustomerAddress] [varchar](100) NULL,
+	[CustomerCity] [varchar](50) NULL,
+	[CustomerState] [varchar](50) NULL,
+	[CustomerZIP] int NULL,
+	[Email] [varchar](100) NULL,
+	[DateOfBirth] date NULL,
+	[PhoneNum] varchar (20) NULL,
+)
+GO
+drop table [WorkingCustomerData]
+
+ALTER table [dbo].[tblCustomer]
+ALTER column [PhoneNumber] varchar (20)
+
+INSERT INTO [WorkingCustomerData] (
+[CustomerFname], [CustomerLname], [CustomerAddress], [CustomerCity], [CustomerState],
+[CustomerZIP], [Email], [DateOfBirth], [PhoneNum])
+SELECT [CustomerFname], [CustomerLname], [CustomerAddress], [CustomerCity], [CustomerState],
+[CustomerZIP], [Email], [DateOfBirth], [PhoneNum]
+FROM [RAW_CUSTOMER]
+
+
+INSERT INTO tblAddress(StreetAddress, City, [State], Zip)
+SELECT CustomerAddress, CustomerCity, CustomerState, CustomerZIP
+FROM [dbo].[WorkingCustomerData]
+
+-- CREATE PROC to get address id
+ALTER PROC uspGetAddressID
+@Street varchar (100),
+@Zipcode int,
+@Add_ID int OUTPUT
+AS
+SET @Add_ID = (SELECT AddressID FROM tblAddress 
+				WHERE StreetAddress = @Street AND Zip = @Zipcode)
+
+-- insert customer info
+DECLARE @Run INT = (SELECT COUNT(*) FROM [WorkingCustomerData])
+DECLARE @ID INT
+DECLARE @CID INT
+DECLARE @FName varchar(50)
+DECLARE @LName varchar(50)
+DECLARE @PNum varchar(50)
+DECLARE @EMAIL varchar(100)
+DECLARE @D_OB date
+DECLARE @AddID INT
+DECLARE @ST varchar(50)
+DECLARE @ZipC int
+
+WHILE @Run > 0
+BEGIN
+SET @ID = (SELECT MIN(CustomerID) FROM [WorkingCustomerData])
+SET @FName = (SELECT [CustomerFname] FROM [WorkingCustomerData] WHERE CustomerID = @ID)
+SET @LName = (SELECT [CustomerLname] FROM [WorkingCustomerData] WHERE CustomerID = @ID)
+SET @PNum = (SELECT [PhoneNum] FROM [WorkingCustomerData] WHERE CustomerID = @ID)
+SET @EMAIL = (SELECT [Email] FROM [WorkingCustomerData] WHERE CustomerID = @ID)
+SET @D_OB = (SELECT [DateOfBirth] FROM [WorkingCustomerData] WHERE CustomerID = @ID)
+SET @AddID = (SELECT AddressID FROM tblAddress WHERE AddressID = @AddID)
+
+EXEC uspGetAddressID
+@Street = @ST,
+@Zipcode = @ZipC,
+@Add_ID = @AddID OUTPUT
+
+BEGIN TRAN G1
+INSERT INTO tblCustomer (FirstName, LastName, PhoneNumber, Email, DOB, AddressID)
+VALUES (@FName, @LName, @PNum, @EMAIL, @D_OB, @AddID)
+SET @CID = (SELECT SCOPE_IDENTITY())
+
+IF @@ERROR <> 0
+ROLLBACK TRAN G1
+ELSE
+COMMIT TRAN G1
+
+SET @Run = @Run -1
+END
+
