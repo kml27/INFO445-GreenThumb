be94a41c3dc827ada6eec7e18db7d8b9d5df3921
Josiah Chapman <josiahc@uw.edu>
2018-04-29 00:13:26 -0700

    added insert detailType and most of insert detail

--- moreSprocs.sql
+++ moreSprocs.sql
@@ -48,3 +48,65 @@ BEGIN
 	RETURN
 END
 GO
+
+USE GREEN_THUMB
+GO
+
+ALTER PROCEDURE josiahc_uspInsertDetailType
+@name VARCHAR(50),
+@desc VARCHAR(500)
+AS
+IF @name IS NULL BEGIN
+	RAISERROR('DetailTypeName (@name) cannot be null', 11, 1)
+	RETURN
+END
+
+BEGIN TRAN T1
+	INSERT INTO tblDetailType(DetailTypeName, DetailTypeDesc)
+		VALUES(@name, @desc)
+	IF @@ERROR <> 0
+		ROLLBACK TRAN T1
+	ELSE
+		COMMIT TRAN T1
+GO
+
+CREATE PROCEDURE josiahc_uspInsertDetail
+@detailTypeName VARCHAR(50),
+@unit VARCHAR(500),
+@detailDesc VARCHAR(500)
+AS
+IF @detailDesc IS NULL BEGIN
+	RAISERROR('DetailDesc (@detailDesc) cannot be null', 11, 1)
+	RETURN
+END
+
+DECLARE @DTID INT
+DECLARE @OID INT
+DECLARE @UID INT
+
+EXEC josiahc_uspGetDetailTypeID
+@typeName = @detailTypeName,
+@detailTypeID = @DTID OUTPUT
+IF @DTID IS NULL BEGIN
+	RAISERROR ('Could not find that detail type', 11,1)
+	RETURN
+END
+
+EXEC josiahc_uspGetUnitID
+@unitName = @unit,
+@unitID = @UID OUTPUT
+IF @UID IS NULL BEGIN
+	RAISERROR ('Could not find that unit', 11,1)
+	RETURN
+END
+
+-- get offeringID goes here
+
+BEGIN TRAN T1
+	INSERT INTO tblDetail(DetailTypeID, OfferingID, UnitID, DetailDesc)
+		VALUES(@DTID, @OID, @UID, @detailDesc)
+	IF @@ERROR <> 0
+		ROLLBACK TRAN T1
+	ELSE
+		COMMIT TRAN T1
+GO
