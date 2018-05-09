b2394c522b85733342d170497eb10440c2762905
jchou8 <joseph98c@gmail.com>
2018-05-09 00:58:52 -0700

    added better sprocs and code for populating tables

--- jchou8_Populate.sql
+++ jchou8_Populate.sql
@@ -9,3 +9,154 @@ VALUES ('0.5 stars'),
 ('4 stars'),
 ('4.5 stars'),
 ('5 stars')
+
+GO
+
+
+/*CREATE*/ ALTER PROCEDURE jchou8_uspInsertOfferingWrapper
+@Run INT
+AS
+DECLARE @SellID INT 
+DECLARE @ProdID INT
+DECLARE @AddressID INT
+
+DECLARE @SellFname varchar(100)
+DECLARE @SellLname varchar(100)
+DECLARE @SellDOB DATE
+
+DECLARE @Prod varchar(100)
+
+DECLARE @AddressSt varchar(100)
+DECLARE @AddressCity varchar(100)
+DECLARE @AddressState varchar(100)
+DECLARE @AddressZip INT
+
+DECLARE @Price money
+
+DECLARE @OfferingName varchar(50)
+DECLARE @StartDate DATE = GETDATE()
+
+WHILE @Run > 0
+BEGIN
+	SET @SellID = (SELECT TOP 1 CustomerID FROM tblCustomer ORDER BY NEWID())
+	SET @ProdID = (SELECT TOP 1 ProductID FROM tblProduct ORDER BY NEWID())
+	SET @AddressID = (SELECT TOP 1 AddressID FROM tblAddress ORDER BY NEWID())
+
+	WHILE NOT EXISTS (
+		SELECT * FROM tblCustomerCustomerType CCT
+		JOIN tblCustomerType CT ON CCT.CustTypeID = CT.CustTypeID
+		WHERE CT.CustTypeName = 'Seller'
+		AND CCT.CustomerID = @SellID
+	) BEGIN
+		 SET @SellID = (SELECT TOP 1 CustomerID FROM tblCustomer ORDER BY NEWID())
+	END
+	
+	SET @SellFname = (SELECT FirstName FROM tblCustomer WHERE CustomerID = @SellID)
+	SET @SellLname = (SELECT LastName FROM tblCustomer WHERE CustomerID = @SellID)
+	SET @SellDOB = (SELECT DOB FROM tblCustomer WHERE CustomerID = @SellID)
+
+	SET @Prod = (SELECT ProductName FROM tblProduct WHERE ProductID = @ProdID)
+	
+	SET @AddressSt = (SELECT StreetAddress FROM tblAddress WHERE AddressID = @AddressID)
+	SET @AddressCity = (SELECT City FROM tblAddress WHERE AddressID = @AddressID)
+	SET @AddressState = (SELECT [State] FROM tblAddress WHERE AddressID = @AddressID)
+	SET @AddressZip = (SELECT Zip FROM tblAddress WHERE AddressID = @AddressID)
+
+	SET @Price = CONVERT(MONEY, RAND() * 50)
+
+	SET @OfferingName = LEFT(@Prod + ' from ' + @SellFname + ' ' + @SellLname, 50)
+
+	EXEC jchou8_uspInsertOffering
+	@SellFname = @SellFname,
+	@SellLname = @SellLname,
+	@SellDOB = @SellDOB,
+	@ProdName = @Prod,
+	@AddSt = @AddressSt,
+	@AddCity = @AddressCity,
+	@AddState = @AddressState,
+	@AddZip = @AddressZip,
+	@Price = @Price,
+	@Name = @OfferingName,
+	@Start = @StartDate
+
+	SET @Run = @Run - 1
+END
+
+GO
+
+EXEC jchou8_uspInsertOfferingWrapper
+@Run = 5000
+
+GO
+
+CREATE PROCEDURE jchou8_uspSimulateOrder
+@Run INT
+AS
+DECLARE @CustID INT 
+DECLARE @OrderID INT
+DECLARE @OfferingID INT
+
+DECLARE @CustFname varchar(100)
+DECLARE @CustLname varchar(100)
+DECLARE @CustDOB DATE
+
+DECLARE @SellFname varchar(100)
+DECLARE @SellLname varchar(100)
+DECLARE @SellDOB DATE
+
+DECLARE @NumLineItems INT
+DECLARE @Qty INT
+DECLARE @OrdDateTime DATETIME
+
+DECLARE @OffName varchar(50)
+DECLARE @OffStart DATE
+
+WHILE @Run > 0
+BEGIN
+	SET @CustID = (SELECT TOP 1 CustomerID FROM tblCustomer ORDER BY NEWID())
+	
+	SET @CustFname = (SELECT FirstName FROM tblCustomer WHERE CustomerID = @CustID)
+	SET @CustLname = (SELECT LastName FROM tblCustomer WHERE CustomerID = @CustID)
+	SET @CustDOB = (SELECT DOB FROM tblCustomer WHERE CustomerID = @CustID)
+
+	SET @NumLineItems = RAND() * 5 + 1
+	SET @OrdDateTime = CURRENT_TIMESTAMP
+
+	EXEC jchou8_uspInsertAndReturnOrder
+	@CustFname = @CustFname,
+	@CustLname = @CustLname,
+	@CustDOB = @CustDOB,
+	@DateTime = @OrdDateTime,
+	@ORID = @OrderID OUTPUT
+
+	WHILE @NumLineItems > 0
+	BEGIN
+		SET @OfferingID = (SELECT TOP 1 OfferingID FROM tblOffering ORDER BY NEWID())
+
+		SET @SellFname = (SELECT C.FirstName FROM tblOffering O JOIN tblCustomer C ON O.SellerID = C.CustomerID WHERE O.OfferingID = @OfferingID)
+		SET @SellLname = (SELECT C.LastName FROM tblOffering O JOIN tblCustomer C ON O.SellerID = C.CustomerID WHERE O.OfferingID = @OfferingID)
+		SET @SellDOB = (SELECT C.DOB FROM tblOffering O JOIN tblCustomer C ON O.SellerID = C.CustomerID WHERE O.OfferingID = @OfferingID)
+		SET @OffName = (SELECT OfferingName FROM tblOFFERING WHERE OfferingID = @OfferingID) 
+		SET @OffStart = (SELECT StartDate FROM tblOFFERING WHERE OfferingID = @OfferingID) 
+		
+		SET @Qty = RAND() * 10 + 1
+
+		EXEC jchou8_uspInsertLineItemWithID
+		@SellFname = @SellFname,
+		@SellLname = @SellLname,
+		@SellDOB = @SellDOB,
+		@OffName = @OffName,
+		@OffStart = @OffStart,
+		@ORID = @OrderID,
+		@Quantity = @Qty
+
+		SET @NumLineItems = @NumLineItems - 1
+	END
+
+	SET @Run = @Run - 1
+END
+
+GO
+
+EXEC jchou8_uspSimulateOrder
+@Run = 5000
--- jchou8_insertSprocs.sql
+++ jchou8_insertSprocs.sql
@@ -15,9 +15,9 @@ DECLARE @SID INT
 DECLARE @RID INT
 
 EXEC emilyd61_uspGetCustID
-@Fname = @CustFname
-@Lname = @CustLname
-@Dob = @CustDOB
+@Fname = @CustFname,
+@Lname = @CustLname,
+@Dob = @CustDOB,
 @CustID = @CID OUTPUT
 
 IF @CID IS NULL
@@ -28,9 +28,9 @@ IF @CID IS NULL
 END
 
 EXEC emilyd61_uspGetCustID
-@Fname = @SellFname
-@Lname = @SellLname
-@Dob = @SellDOB
+@Fname = @SellFname,
+@Lname = @SellLname,
+@Dob = @SellDOB,
 @CustID = @SID OUTPUT
 
 IF @SID IS NULL
@@ -90,9 +90,9 @@ AS
 DECLARE @CID INT
 
 EXEC emilyd61_uspGetCustID
-@Fname = @CustFname
-@Lname = @CustLname
-@Dob = @CustDOB
+@Fname = @CustFname,
+@Lname = @CustLname,
+@Dob = @CustDOB,
 @CustID = @CID OUTPUT
 
 IF @CID IS NULL
@@ -112,7 +112,41 @@ ELSE
 
 GO
 
-/* Given customer information, order datetime, seller information, quantity, */
+
+/* Inserts an order given customer info and order datetime, then returns the newly inserted order ID */
+CREATE PROCEDURE jchou8_uspInsertAndReturnOrder
+@CustFname varchar(100),
+@CustLname varchar(100),
+@CustDOB date,
+@DateTime datetime,
+@ORID INT OUTPUT
+AS
+DECLARE @CID INT
+
+EXEC emilyd61_uspGetCustID
+@Fname = @CustFname,
+@Lname = @CustLname,
+@Dob = @CustDOB,
+@CustID = @CID OUTPUT
+
+IF @CID IS NULL
+	BEGIN
+	PRINT '@CID IS NULL and will fail on insert statement; process terminated'
+	RAISERROR ('CustomerID variable @CID cannot be NULL', 11,1)
+	RETURN
+END
+
+BEGIN TRAN T1
+INSERT INTO tblOrder(CustomerID, OrderDateTime)
+VALUES (@CID, @DateTime)
+IF @@ERROR <> 0
+	ROLLBACK TRAN T1
+ELSE 
+	SET @ORID = (SELECT SCOPE_IDENTITY())
+	COMMIT TRAN T1
+GO
+
+/* Given customer information, order datetime, seller information, and quantity, inserts a line item. */
 CREATE PROCEDURE jchou8_uspInsertLineItem
 @CustFname varchar(100),
 @CustLname varchar(100),
@@ -131,9 +165,54 @@ DECLARE @OFID INT
 EXEC long27km_GetOrderID
 @FName = @CustFname,
 @Lname = @CustLname,
-@DOB = @CustDOB
+@DOB = @CustDOB,
 @OrderDateTime = @DateTime,
 @OrderID = @ORID OUTPUT
+
+IF @ORID IS NULL
+	BEGIN
+	PRINT '@ORID IS NULL and will fail on insert statement; process terminated'
+	RAISERROR ('OrderID variable @ORID cannot be NULL', 11,1)
+	RETURN
+END
+
+EXEC jchou8_uspGetOffering
+@SellFname = @SellFname,
+@SellLname = @SellLname,
+@SellDOB = @SellDOB,
+@Name = @OffName,
+@Start = @OffStart,
+@OffID = @OFID OUTPUT
+
+IF @OFID IS NULL
+	BEGIN
+	PRINT '@OFID IS NULL and will fail on insert statement; process terminated'
+	RAISERROR ('OfferingID variable @OFID cannot be NULL', 11,1)
+	RETURN
+END
+
+BEGIN TRAN T1
+INSERT INTO tblLineItem(OrderID, OfferingID, Qty)
+VALUES(@ORID, @OFID, @Quantity)
+IF @@ERROR <> 0
+	ROLLBACK TRAN T1
+ELSE 
+	COMMIT TRAN T1
+
+GO
+
+
+/* Given order ID, seller information, and quantity, inserts a line item. */
+CREATE PROCEDURE jchou8_uspInsertLineItemWithID
+@SellFname varchar(100),
+@SellLname varchar(100),
+@SellDOB date,
+@OffName varchar(50),
+@OffStart DATE,
+@ORID INT,
+@Quantity INT
+AS
+DECLARE @OFID INT
 
 IF @ORID IS NULL
 	BEGIN
@@ -174,8 +253,10 @@ CREATE PROCEDURE jchou8_uspInsertOffering
 @SellLname varchar(100),
 @SellDOB date,
 @ProdName varchar(100),
-@StAddress varchar(100),
-@Zip INT,
+@AddSt varchar(100),
+@AddCity varchar(100),
+@AddState varchar(100),
+@AddZip INT,
 @Price money,
 @Name varchar(50),
 @Desc varchar(2500) = NULL,
@@ -187,9 +268,9 @@ DECLARE @PID INT
 DECLARE @AID INT
 
 EXEC emilyd61_uspGetCustID
-@Fname = @SellFname
-@Lname = @SellLname
-@Dob = @SellDOB
+@Fname = @SellFname,
+@Lname = @SellLname,
+@Dob = @SellDOB,
 @CustID = @SID OUTPUT
 
 IF @SID IS NULL
@@ -200,7 +281,7 @@ IF @SID IS NULL
 END
 
 EXEC emilyd61_uspGetProdID
-@ProdName = @ProdName
+@ProdName = @ProdName,
 @ProdID = @PID OUTPUT
 
 IF @PID IS NULL
@@ -211,9 +292,11 @@ IF @PID IS NULL
 END
 
 EXEC emilyd61_uspGetAddressID
-@Street = @StAddress,
-@Zipcode = @Zip,
-@Add_ID = @AID OUTPUT
+@Street = @AddSt,
+@City = @AddCity,
+@State = @AddState,
+@Zipcode = @AddZip,
+@Address_ID = @AID OUTPUT
 
 IF @AID IS NULL
 	BEGIN
@@ -246,9 +329,9 @@ DECLARE @SID INT
 DECLARE @PID INT
 
 EXEC emilyd61_uspGetCustID
-@Fname = @SellFname
-@Lname = @SellLname
-@Dob = @SellDOB
+@Fname = @SellFname,
+@Lname = @SellLname,
+@Dob = @SellDOB,
 @CustID = @SID OUTPUT
 
 SET @OffID = (
