81dbcb69441369ad8aeccb71740ecf6eedb81e1a
Josiah Chapman <josiahc@uw.edu>
2018-05-27 14:32:45 -0700

    added my dbos

--- Project 6 Stored Procedures_Check Constraints_Computed Columns and Views.sql
+++ Project 6 Stored Procedures_Check Constraints_Computed Columns and Views.sql
@@ -125,13 +125,13 @@ CHECK (dbo.fn_minOrderPay10() = 0)
 /*3) Computed column*/
 -- Emily Ding
 /*Customer age*/
-CREATE FUNCTION fn_customerAge(@CustID INT)
+ALTER FUNCTION fn_customerAge(@CustID INT)
 RETURNS INT
 AS
 BEGIN
 	DECLARE @Ret INT
 	SET @Ret = (SELECT DATEDIFF(YEAR, DOB, GETDATE()) AS Age
-				FROM tblCustomer C)
+				FROM tblCustomer C WHERE CustomerID = @CustID)
 RETURN @Ret
 END
 -- Alter table
@@ -139,19 +139,18 @@ ALTER TABLE tblCustomer
 ADD CustAge AS (dbo.fn_customerAge(CustomerID))
 
 /*Sell Tax rate: Seattle, it's 10.1% */
-CREATE FUNCTION fn_SalesTax(@OffID INT)
+ALTER FUNCTION fn_SalesTax(@OffID INT)
 RETURNS INT
 AS
 BEGIN
 	DECLARE @Ret INT
 	SET @Ret = (SELECT Price * .101 AS TaxFee
-				FROM tblOffering)
+				FROM tblOffering WHERE OfferingID = @OffID)
 RETURN @Ret
 END
 -- Alter table
 ALTER TABLE tblOffering
 Add SellTaxFee AS (dbo.fn_SalesTax(OfferingID))
-
 
 /*4) View*/
 -- Emily Ding
@@ -203,3 +202,411 @@ WHERE RA.RatingName = '5 stars' AND CT.CustTypeName = 'Loyal' AND
 					HAVING SUM(O.Price) > 10000)
 GROUP BY C.CustomerID
 
+/*
+  _                    _    
+ | |                  | |   
+ | |__  _ __ ___  __ _| | __
+ | '_ \| '__/ _ \/ _` | |/ /
+ | |_) | | |  __/ (_| |   < 
+ |_.__/|_|  \___|\__,_|_|\_\
+
+*/
+
+-- Start of Josiah's work ->
+-- views
+-- 1) Average price of each of the top 100 product sold on the platform ordered by most popular product
+CREATE VIEW josiahc_vwAvgPriceOfTop100Prod AS
+SELECT TOP 100 P.ProductName, PRI.[Average Price]
+FROM tblProduct P
+	JOIN tblOffering O ON O.ProductID = P.ProductID
+	JOIN (SELECT P.ProductID, COUNT(*) AS 'Popularity'
+			FROM tblProduct P
+				JOIN tblOffering O ON O.ProductID = P.ProductID
+			GROUP BY P.ProductID) POP ON POP.ProductID = P.ProductID
+	JOIN (SELECT P.ProductID, AVG(O.Price) AS 'Average Price'
+			FROM tblProduct P
+				JOIN tblOffering O ON O.ProductID = P.ProductID
+			GROUP BY P.ProductID) PRI ON PRI.ProductID = P.ProductID
+	GROUP BY P.ProductID, P.ProductName, POP.Popularity, PRI.[Average Price]
+ORDER BY POP.Popularity DESC
+
+-- 2) Ratio of offerings made to orders placed for each customer who is also a seller, ordered by more offerings made
+CREATE VIEW josiahc_vwRatioOfOfferingsToOrders AS
+SELECT TOP 100000 C.FirstName, C.LastName, (OFFR.[Offerings Made] / ORD.[Orders Placed]) AS 'Ratio: Offerings - Orders'
+FROM tblCustomer C
+	JOIN (SELECT C.CustomerID, COUNT(*) AS 'Offerings Made'
+			FROM tblCustomer C
+				JOIN tblOffering O ON C.CustomerID = O.SellerID
+			GROUP BY C.CustomerID) OFFR ON OFFR.CustomerID = C.CustomerID
+	JOIN (SELECT C.CustomerID, COUNT(*) AS 'Orders Placed'
+			FROM tblCustomer C
+				JOIN tblOrder O ON C.CustomerID = O.CustomerID
+			GROUP BY C.CustomerID) ORD ON ORD.CustomerID = C.CustomerID
+GROUP BY C.CustomerID, C.FirstName, C.LastName, ORD.[Orders Placed], OFFR.[Offerings Made]
+ORDER BY (OFFR.[Offerings Made] / ORD.[Orders Placed]) DESC
+
+-- check constraints
+-- 1) Two Detail Types cannot have the same name
+CREATE FUNCTION josiahc_fnNoDuplicateNames()
+RETURNS INT
+AS BEGIN
+	DECLARE @Ret INT = 0
+
+	IF EXISTS 
+		(SELECT DetailTypeName
+		FROM tblDetailType
+		GROUP BY DetailTypeName
+		HAVING COUNT(*) > 1)
+	SET @Ret = 1
+
+	RETURN @Ret
+END
+GO
+
+ALTER TABLE tblDetailType
+ADD CONSTRAINT josiahc_ckNoDuplicateNames
+CHECK (dbo.josiahc_fnNoDuplicateNames() = 0)
+GO
+
+-- 2) An offering cannot have more than one of the same Detail Type
+CREATE FUNCTION josiahc_fnNoDuplicateUnitNames()
+RETURNS INT
+AS BEGIN
+	DECLARE @Ret INT = 0
+
+	IF EXISTS 
+		(SELECT UnitName
+		FROM tblUnit
+		GROUP BY UnitName
+		HAVING COUNT(*) > 1)
+	SET @Ret = 1
+
+	RETURN @Ret
+END
+GO
+
+ALTER TABLE tblUnit
+ADD CONSTRAINT josiahc_ckNoDuplicateUnitNames
+CHECK (dbo.josiahc_fnNoDuplicateUnitNames() = 0)
+GO
+
+-- computed columns
+-- 1) average rating
+CREATE FUNCTION josiahc_fnCalcAvgRating(@SellID INT)
+RETURNS NUMERIC(3, 1)
+AS BEGIN
+	DECLARE @Result NUMERIC(3, 1)
+	SET @Result = (SELECT AVG(CAST(
+		CASE SUBSTRING(R.RatingName, 2, 1)
+			WHEN '.' THEN SUBSTRING(R.RatingName, 1, 3) 
+			ELSE SUBSTRING(R.RatingName, 1, 1) END
+		AS NUMERIC(3, 1)))
+		FROM tblRating R
+			JOIN tblReview RV ON RV.RatingID = R.RatingID
+		WHERE sellerID = @SellID)
+	Return @Result
+END
+GO
+
+ALTER TABLE tblCustomer
+ADD AvgRating AS (dbo.josiahc_fnCalcAvgRating(CustomerID))
+
+-- 2) total number of offerings by seller
+CREATE FUNCTION josiahc_fnCalcNumOfOfferings(@CustID INT)
+RETURNS INT
+AS BEGIN
+	DECLARE @Result INT
+	SET @Result = (SELECT COUNT(*)
+		FROM tblCustomer C
+			JOIN tblOffering O ON C.CustomerID = O.SellerID
+		WHERE C.CustomerID = @CustID)
+	RETURN @Result
+END
+GO
+
+ALTER TABLE tblCustomer
+ADD NumOfOfferings AS (dbo.josiahc_fnCalcNumOfOfferings(CustomerID))
+
+-- stored procedures
+-- context for sproc 1 {
+	-- extra product types are needed
+	INSERT INTO tblProductType (ProductTypeName, ProductTypeDesc)
+		VALUES('Compost & Rainbarrel', 'Various compost and rainbarrel products')
+
+	INSERT INTO tblProductType (ProductTypeName, ProductTypeDesc)
+		VALUES('Watering Products', 'Products to water your plants')
+	
+	INSERT INTO tblProductType (ProductTypeName, ProductTypeDesc)
+		VALUES('Hydroponics', 'For all your soilless gardening needs')
+	
+	INSERT INTO tblProductType (ProductTypeName, ProductTypeDesc)
+		VALUES('Greenhouse', 'Greenhouse gear')
+
+	-- make the tables
+	CREATE TABLE WORKING_Details
+		(DetailID INT IDENTITY(1,1) PRIMARY KEY,
+		DetailType [varchar](999) NULL,
+		[Detail] [varchar](999) NULL,
+		[source_JobURL] [varchar](999) NULL)
+	GO
+
+	CREATE TABLE WORKING_Products
+		(ProductID INT IDENTITY(1,1) PRIMARY KEY,
+		ProductName VARCHAR(999),
+		ProductTypeID INT,
+		ProductDesc VARCHAR(999),
+		Price VARCHAR(100))
+	GO
+
+	-- insert into working products
+	INSERT INTO WORKING_Products (ProductName, ProductDesc, Price)
+	SELECT product_title, JobURL, price
+	FROM raw_compost_and_rainbarrel_products
+
+	UPDATE WORKING_Products 
+	SET ProductTypeID = 14
+	WHERE ProductTypeID IS NULL
+
+	INSERT INTO WORKING_Products (ProductName, ProductDesc, Price)
+	SELECT product_title, JobURL, price
+	FROM RAW_gardening_tools_products
+
+	UPDATE WORKING_Products 
+	SET ProductTypeID = 8
+	WHERE ProductTypeID IS NULL
+
+	INSERT INTO WORKING_Products (ProductName, ProductDesc, Price)
+	SELECT product_title, JobURL, price
+	FROM Raw_Watering_Products
+
+	UPDATE WORKING_Products 
+	SET ProductTypeID = 15
+	WHERE ProductTypeID IS NULL
+
+	INSERT INTO WORKING_Products (ProductName, ProductDesc, Price)
+	SELECT product_title, JobURL, price
+	FROM RAW_seeds_products
+
+	UPDATE WORKING_Products 
+	SET ProductTypeID = 11
+	WHERE ProductTypeID IS NULL
+
+	INSERT INTO WORKING_Products (ProductName, ProductDesc, Price)
+	SELECT product_title, JobURL, price
+	FROM RAW_hydroponics_products
+
+	UPDATE WORKING_Products 
+	SET ProductTypeID = 16
+	WHERE ProductTypeID IS NULL
+
+	INSERT INTO WORKING_Products (ProductName, ProductDesc, Price)
+	SELECT product_title, JobURL, price
+	FROM RAW_pots_and_planters_products_with_url
+
+	UPDATE WORKING_Products 
+	SET ProductTypeID = 10
+	WHERE ProductTypeID IS NULL
+
+	INSERT INTO WORKING_Products (ProductName, ProductDesc, Price)
+	SELECT product_title, JobURL, price
+	FROM RAW_greenhouse_products
+
+	UPDATE WORKING_Products 
+	SET ProductTypeID = 17
+	WHERE ProductTypeID IS NULL
+
+	-- insert into working details
+	INSERT INTO WORKING_Details (Detail, DetailType, source_JobURL)
+	SELECT DetailType, DetailValue, source_JobURL
+	FROM random_gardening_tools_details
+
+	INSERT INTO WORKING_Details (Detail, DetailType, source_JobURL)
+	SELECT DetailType, DetailValue, source_JobURL
+	FROM new_random_pots_and_planters_details
+
+	INSERT INTO WORKING_Details (Detail, DetailType, source_JobURL)
+	SELECT DetailType, DetailValue, source_JobURL
+	FROM Mixed_Product_Details
+
+	-- some fields were blank
+	DELETE FROM WORKING_Details
+	WHERE Detail = '' OR DetailType = ''
+
+	-- insert into detailType and update workingDetail to corresponding FK
+	INSERT INTO tblDetailType (DetailTypeName)
+	SELECT DISTINCT DetailType
+	FROM WORKING_Details
+
+	UPDATE WORKING_Details
+	SET DetailType = DetailTypeID
+	FROM WORKING_Details WD
+		JOIN tblDetailType DT ON WD.DetailType = DT.DetailTypeName
+	-- construct and populate temp tables
+	CREATE TABLE Temp_Details
+		(DetailID INT IDENTITY(1,1) PRIMARY KEY,
+		DetailType INT NULL,
+		[Detail] [varchar](500) NULL,
+		[source_JobURL] [varchar](500) NULL)
+	GO
+
+	CREATE TABLE Temp_Products
+		(ProductID INT IDENTITY(1,1) PRIMARY KEY,
+		ProductName VARCHAR(100),
+		ProductTypeID INT,
+		ProductDesc VARCHAR(500),
+		Price VARCHAR(100))
+	GO
+
+	INSERT INTO Temp_Details(DetailType, Detail, source_JobURL)
+	SELECT DetailType, Detail, source_JobURL
+	FROM WORKING_Details
+	GO
+
+	INSERT INTO Temp_Products(ProductTypeID, ProductName, ProductDesc, Price)
+	SELECT ProductTypeID, ProductName, ProductDesc, Price
+	FROM WORKING_Products
+	GO
+-- } end context for sproc
+
+-- sproc 1 josiahc
+ALTER PROCEDURE josiahc_uspPopScrapedDate
+AS
+
+-- product info
+DECLARE @ProdTypeID INT
+DECLARE @ProdName VARCHAR(100)
+DECLARE @ProdDesc VARCHAR(500)
+
+-- detail info
+DECLARE @URL VARCHAR(500)
+DECLARE @DetTypeID INT
+DECLARE @OffID INT
+DECLARE @DetDesc VARCHAR(500)
+DECLARE @UnitID INT = (SELECT UnitID FROM tblUnit WHERE UnitName = 'none')
+
+-- offering info
+DECLARE @SellerID INT
+DECLARE @ProdID INT
+DECLARE @AddrID INT
+DECLARE @Price MONEY
+DECLARE @Prices VARCHAR(100)
+DECLARE @OffName VARCHAR(50)
+DECLARE @OffDesc VARCHAR(2500)
+DECLARE @StartDate DATE
+DECLARE @EndDate DATE
+
+-- other vars
+DECLARE @WorkingDetailID INT
+DECLARE @CurrentProdID INT = (SELECT MIN(ProductID) FROM Temp_Products)
+DECLARE @MaxProdID INT = (SELECT MAX(ProductID) FROM Temp_Products)
+
+WHILE @CurrentProdID <= @MaxProdID BEGIN
+	PRINT @CurrentProdID
+	-- prep product vars
+	SET @ProdTypeID = (SELECT ProductTypeID FROM Temp_Products WHERE ProductID = @CurrentProdID)
+	SET @ProdName = (SELECT ProductName FROM Temp_Products WHERE ProductID = @CurrentProdID)
+	SET @ProdDesc = (SELECT ProductDesc FROM Temp_Products WHERE ProductID = @CurrentProdID)
+
+	-- prep offering vars
+	SET @SellerID =
+		(SELECT TOP(1) C.CustomerID
+		FROM tblCustomerType CT
+			JOIN tblCustomerCustomerType CCT ON CCT.CustTypeID = CT.CustTypeID
+			JOIN tblCustomer C ON C.CustomerID = CCT.CustomerID
+		WHERE CT.CustTypeName = 'seller'
+			AND CCT.EndDate IS NULL
+		ORDER BY NEWID())
+	SET @AddrID =
+		(SELECT TOP(1) AddressID
+		FROM tblAddress
+		ORDER BY NEWID())
+	SET @OffName = (SELECT 'offering from ' + (SELECT FirstName FROM tblCustomer WHERE CustomerID = @SellerID))
+	SET @OffDesc = NULL
+	SET @StartDate = (SELECT CAST(DATEADD(day, -CAST(RAND() * 10000 AS INT), GETDATE()) AS DATE))
+	SET @EndDate = (SELECT DATEADD(day, CAST(RAND() * 100 AS INT), @StartDate))
+	PRINT 'start on prices'
+	SET @Prices = (SELECT Price FROM Temp_Products WHERE ProductID = @CurrentProdID)
+	IF @Prices <> '' AND @Prices NOT LIKE '%¢%'
+		SET @Price = (SELECT CAST(SUBSTRING(@Prices, CHARINDEX('$', @Prices, 2), 10) AS MONEY))
+	ELSE
+		SET @Price = (SELECT CAST(((RAND() * 200) + 1) AS MONEY))
+	PRINT 'finish prices'
+
+	-- prep common detail vars
+	SET @URL = @ProdDesc
+
+	PRINT 'Begin Trans'
+	BEGIN TRAN T1
+		-- insert product
+		INSERT INTO tblProduct(ProductName, ProductDesc, ProductTypeID)
+			VALUES(@ProdName, @ProdDesc, @ProdTypeID)
+		IF @@ERROR <> 0
+			ROLLBACK TRAN T1
+		ELSE
+			BEGIN TRAN T2
+				-- insert offering
+				SET @ProdID = SCOPE_IDENTITY()
+				INSERT INTO tblOffering(SellerID, ProductID, AddressID, OfferingName, OfferingDesc, StartDate, EndDate, Price)
+					VALUES(@SellerID, @ProdID, @AddrID, @OffName, @OffDesc, @StartDate, @EndDate, @Price)
+				IF @@ERROR <> 0
+					ROLLBACK TRAN T2
+				ELSE
+					BEGIN TRAN T3
+						-- insert details
+						SET @OffID = SCOPE_IDENTITY()
+
+						-- create temprorary table for details attached to product
+						SELECT * 
+						INTO Ephemeral_Details
+						FROM Temp_Details
+						WHERE source_JobURL = @URL
+
+						-- check to see if any details exist for the given product, if not, do nothing
+						WHILE (SELECT COUNT(*) FROM Ephemeral_Details) > 0 BEGIN
+							SET @WorkingDetailID = (SELECT TOP(1) DetailID FROM Ephemeral_Details ORDER BY DetailID ASC)
+							SET @DetDesc = (SELECT Detail FROM Ephemeral_Details WHERE DetailID = @WorkingDetailID)
+							SET @DetTypeID = CAST((SELECT DetailType FROM Ephemeral_Details WHERE DetailID = @WorkingDetailID) AS INT)
+
+							INSERT INTO tblDetail(OfferingID, DetailTypeID, UnitID, DetailDesc)
+								VALUES (@OffID, @DetTypeID, @UnitID, @DetDesc)
+
+							DELETE FROM Ephemeral_Details
+							WHERE DetailID = @WorkingDetailID
+						END
+
+						-- get rid of temp table
+						DROP TABLE Ephemeral_Details
+						IF @@ERROR <> 0
+							ROLLBACK TRAN T3
+						ELSE
+						COMMIT TRAN T3
+			COMMIT TRAN T2
+	COMMIT TRAN T1
+	SET @CurrentProdID = @CurrentProdID + 1
+END
+
+-- 2) insert unit
+CREATE PROCEDURE josiahc_uspInsertUnit
+@UnitName VARCHAR(50),
+@UnitAbbr VARCHAR(10),
+@UnitDesc VARCHAR(500)
+AS
+
+BEGIN TRAN T1
+	INSERT INTO tblUnit(UnitName, UnitDesc, UnitAbbr)
+		VALUES(@UnitName, @UnitDesc, @UnitAbbr)
+	IF @@ERROR <> 0
+		ROLLBACK TRAN T1
+	ELSE
+		COMMIT TRAN T1
+GO
+
+/*
+  _                    _    
+ | |                  | |   
+ | |__  _ __ ___  __ _| | __
+ | '_ \| '__/ _ \/ _` | |/ /
+ | |_) | | |  __/ (_| |   < 
+ |_.__/|_|  \___|\__,_|_|\_\
+
+*/
