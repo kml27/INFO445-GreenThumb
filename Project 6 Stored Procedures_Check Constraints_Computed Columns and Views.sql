a534e0a4da0f1ed3c34f4e4bea225d120780dc42
Emily Ding <emilyd61@uw.edu>
2018-05-28 17:12:14 -0700

    updated the store proc

--- Project 6 Stored Procedures_Check Constraints_Computed Columns and Views.sql
+++ Project 6 Stored Procedures_Check Constraints_Computed Columns and Views.sql
@@ -8,7 +8,8 @@ CREATE PROCEDURE emilyd61_populateOrder
 @F_Name varchar(50),
 @L_Name varchar(50),
 @D_OB DATE,
-@DTime DATETIME
+@DTime DATETIME,
+@OrderID INT OUTPUT
 AS
 DECLARE @CID INT
 
@@ -31,8 +32,10 @@ VALUES (@CID, @DTime)
 
 IF @@ERROR <> 0
  ROLLBACK TRAN G1
-ELSE
- COMMIT TRAN G1
+ELSE 
+ SET @OrderID = (SELECT SCOPE_IDENTITY())
+ COMMIT TRAN T1
+GO
 
 -- Insert customer information from RAW_DATA which converted as WorkingCustomerData
 CREATE PROCEDURE emilyd61_uspInsertCustWapperfromWorkingData
@@ -610,3 +613,255 @@ GO
  |_.__/|_|  \___|\__,_|_|\_\
 
 */
+-- Joseph Chou
+/* Stored procedures */
+/* Given order ID, seller information, and quantity, inserts a line item. */
+ALTER PROCEDURE [dbo].[jchou8_uspInsertLineItemWithID]
+@SellFname varchar(100),
+@SellLname varchar(100),
+@SellDOB date,
+@OffName varchar(50),
+@OffStart DATE,
+@ORID INT,
+@Quantity INT
+AS
+DECLARE @OFID INT
+
+IF @ORID IS NULL
+	BEGIN
+	PRINT '@ORID IS NULL and will fail on insert statement; process terminated'
+	RAISERROR ('OrderID variable @ORID cannot be NULL', 11,1)
+	RETURN
+END
+
+EXEC jchou8_uspGetOffering
+@SellFname = @SellFname,
+@SellLname = @SellLname,
+@SellDOB = @SellDOB,
+@Name = @OffName,
+@Start = @OffStart,
+@OffID = @OFID OUTPUT
+
+IF @OFID IS NULL
+	BEGIN
+	PRINT '@OFID IS NULL and will fail on insert statement; process terminated'
+	RAISERROR ('OfferingID variable @OFID cannot be NULL', 11,1)
+	RETURN
+END
+
+BEGIN TRAN T1
+INSERT INTO tblLineItem(OrderID, OfferingID, Qty)
+VALUES(@ORID, @OFID, @Quantity)
+IF @@ERROR <> 0
+	ROLLBACK TRAN T1
+ELSE 
+	COMMIT TRAN T1
+GO
+
+/* Insert a simulated order with random offerings */
+CREATE PROCEDURE jchou8_uspSimulateOrder
+@Run INT
+AS
+DECLARE @CustID INT 
+DECLARE @OrderID INT
+DECLARE @OfferingID INT
+
+DECLARE @CustFname varchar(100)
+DECLARE @CustLname varchar(100)
+DECLARE @CustDOB DATE
+
+DECLARE @SellFname varchar(100)
+DECLARE @SellLname varchar(100)
+DECLARE @SellDOB DATE
+
+DECLARE @NumLineItems INT
+DECLARE @Qty INT
+DECLARE @OrdDateTime DATETIME
+
+DECLARE @OffName varchar(50)
+DECLARE @OffStart DATE
+
+WHILE @Run > 0
+BEGIN
+	SET @CustID = (SELECT TOP 1 CustomerID FROM tblCustomer ORDER BY NEWID())
+	
+	SET @CustFname = (SELECT FirstName FROM tblCustomer WHERE CustomerID = @CustID)
+	SET @CustLname = (SELECT LastName FROM tblCustomer WHERE CustomerID = @CustID)
+	SET @CustDOB = (SELECT DOB FROM tblCustomer WHERE CustomerID = @CustID)
+
+	SET @NumLineItems = RAND() * 5 + 1
+	SET @OrdDateTime = CURRENT_TIMESTAMP
+
+	EXEC jchou8_uspInsertAndReturnOrder
+	@CustFname = @CustFname,
+	@CustLname = @CustLname,
+	@CustDOB = @CustDOB,
+	@DateTime = @OrdDateTime,
+	@ORID = @OrderID OUTPUT
+
+	WHILE @NumLineItems > 0
+	BEGIN
+		SET @OfferingID = (SELECT TOP 1 OfferingID FROM tblOffering ORDER BY NEWID())
+
+		SET @SellFname = (SELECT C.FirstName FROM tblOffering O JOIN tblCustomer C ON O.SellerID = C.CustomerID WHERE O.OfferingID = @OfferingID)
+		SET @SellLname = (SELECT C.LastName FROM tblOffering O JOIN tblCustomer C ON O.SellerID = C.CustomerID WHERE O.OfferingID = @OfferingID)
+		SET @SellDOB = (SELECT C.DOB FROM tblOffering O JOIN tblCustomer C ON O.SellerID = C.CustomerID WHERE O.OfferingID = @OfferingID)
+		SET @OffName = (SELECT OfferingName FROM tblOFFERING WHERE OfferingID = @OfferingID) 
+		SET @OffStart = (SELECT StartDate FROM tblOFFERING WHERE OfferingID = @OfferingID) 
+		
+		SET @Qty = RAND() * 10 + 1
+
+		EXEC jchou8_uspInsertLineItemWithID
+		@SellFname = @SellFname,
+		@SellLname = @SellLname,
+		@SellDOB = @SellDOB,
+		@OffName = @OffName,
+		@OffStart = @OffStart,
+		@ORID = @OrderID,
+		@Quantity = @Qty
+
+		SET @NumLineItems = @NumLineItems - 1
+	END
+
+	SET @Run = @Run - 1
+END
+
+GO
+
+/* Business rules */
+/* Customers cannot review themselves */
+CREATE FUNCTION fn_NoSelfReview()
+RETURNS INT
+AS
+BEGIN
+DECLARE @Ret INT = 0
+IF EXISTS (
+	SELECT ReviewID
+	FROM tblReview
+	WHERE CustomerID = SellerID
+)
+	SET @Ret = 1
+RETURN @Ret
+END
+
+GO
+
+ALTER TABLE tblReview
+ADD CONSTRAINT CK_NoSelfReview
+CHECK (dbo.fn_NoSelfReview() = 0)
+
+GO
+
+/* Only customers with type 'seller' can have offerings */
+CREATE FUNCTION fn_OnlySellersOffer()
+RETURNS INT
+AS
+BEGIN
+DECLARE @Ret INT = 0
+IF EXISTS (
+	SELECT O.OfferingID
+	FROM tblOffering O
+	JOIN tblCustomer C ON C.CustomerID = O.SellerID
+	WHERE NOT C.CustomerID IN (
+		SELECT C.CustomerID
+		FROM tblCustomer C
+		JOIN tblCustomerCustomerType CCT ON CCT.CustomerID = C.CustomerID
+		JOIN tblCustomerType CT ON CT.CustTypeID = CCT.CustTypeID
+		WHERE CT.CustTypeName = 'Seller'
+	)
+)
+	SET @Ret = 1
+RETURN @Ret
+END
+
+GO
+
+ALTER TABLE tblOffering
+ADD CONSTRAINT CK_OnlySellersOffer
+CHECK (dbo.fn_OnlySellersOffer() = 0)
+
+GO
+
+/* Computed columns */
+/* Line item subtotal */
+CREATE FUNCTION fn_LineItemSubtotal(@LineItemID INT)
+RETURNS Money
+AS
+BEGIN
+DECLARE @Ret Money
+SET @Ret = (SELECT O.Price * LI.Qty
+FROM tblLineItem LI
+JOIN tblOffering O ON LI.OfferingID = O.OfferingID
+WHERE LI.LineItemID = @LineItemID
+)
+RETURN @Ret
+END
+
+GO
+
+ALTER TABLE tblLineItem
+ADD Subtotal AS (dbo.fn_LineItemSubtotal(LineItemID))
+
+GO
+
+/* Order total */
+CREATE FUNCTION fn_OrderTotal(@OrderID INT)
+RETURNS Money
+AS
+BEGIN
+DECLARE @Ret Money
+SET @Ret = (SELECT SUM(LI.Subtotal)
+FROM tblOrder O
+JOIN tblLineItem LI ON LI.OrderID = O.OrderID
+WHERE O.OrderID = @OrderID
+)
+RETURN @Ret
+END
+
+GO
+
+ALTER TABLE tblOrder
+ADD Total AS (dbo.fn_OrderTotal(OrderID))
+
+GO
+
+/* Views */
+/* View the top seller in each state based on total profit made in the past month */
+CREATE VIEW jchou8_topSellersPerState AS
+SELECT C.CustomerID, C.FirstName, C.LastName, C.PastMonthProfits, S.State
+FROM (SELECT DISTINCT State FROM tblAddress) S
+CROSS APPLY (
+	SELECT TOP 1 C.CustomerID, C.FirstName, C.LastName, C.State, C.PastMonthProfits
+	FROM (
+		SELECT C.CustomerID, C.FirstName, C.LastName, A.State, SUM(R.Total) AS PastMonthProfits
+		FROM tblCustomer C
+		JOIN tblOffering O ON O.SellerID = C.CustomerID
+		JOIN tblLineItem LI ON LI.OfferingID = O.OfferingID
+		JOIN tblOrder R ON R.OrderID = LI.OrderID
+		JOIN tblAddress A ON C.AddressID = A.AddressID
+		WHERE R.OrderDateTime > (SELECT GetDate() - 31)
+		GROUP BY C.FirstName, C.LastName, C.CustomerID, A.State
+	) C
+	WHERE C.State = S.State
+	ORDER BY C.PastMonthProfits DESC
+) C
+
+GO
+
+/* View the customers who have purchased from at least 10 different sellers who have also left at least 10 reviews */
+CREATE VIEW jchou8_diverseBuyersWith10Reviews AS
+SELECT C.CustomerID, C.FirstName, C.LastName,  COUNT(DISTINCT S.CustomerID) AS DistinctSellers
+FROM tblCustomer C
+JOIN tblOrder R ON R.CustomerID = C.CustomerID
+JOIN tblLineItem LI ON LI.OrderID = R.OrderID
+JOIN tblOffering O ON O.OfferingID = LI.OfferingID
+JOIN tblCustomer S ON O.SellerID = S.CustomerID
+WHERE C.CustomerID IN (
+	SELECT C.CustomerID
+	FROM tblCustomer C
+	JOIN tblReview R ON R.CustomerID = C.CustomerID
+	GROUP BY C.CustomerID, C.FirstName, C.LastName
+	HAVING COUNT(R.ReviewID) >= 10
+)
+GROUP BY C.CustomerID, C.FirstName, C.LastName
+HAVING COUNT(DISTINCT S.CustomerID) >= 10
